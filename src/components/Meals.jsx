c2cf2cb225494621198665dd376c4084461b59e6
mohitmalik332 <malikmohit332@gmail.com.com>
2024-11-04 01:50:11 +0530

    sending a POST request with ordered data.

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,26 +1,45 @@
 import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
 
+
+const requestConfig = {};
 
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    // const [loadedMeals, setLoadedMeals] = useState([]);
 
-
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
+// we have made a separate reusable file (useHttp) for the below code...
+    // useEffect(() => { 
+    //     async function fetchMeals() {
+    //         const response = await fetch('http://localhost:3000/meals');
     
-            if(!response.ok){
-                //....
-            }
+    //         if(!response.ok){
+    //             //....
+    //         }
     
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
+    //         const meals = await response.json();
+    //         setLoadedMeals(meals);
+    //     }
 
-        fetchMeals()
-    }, [])
+    //     fetchMeals()
+    // }, [])
 
+
+    const { 
+        data: loadedMeals, 
+        isLoading, 
+        error,
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+
+    if (isLoading){
+        return <p className="center">Fetching meals...</p>
+    }
+
+    // An alternative (temporary) for not using requestConfig.
+    // if (!data){
+    //     return <p>No Meals Found...</p>
+    // }
 
 
     return (
