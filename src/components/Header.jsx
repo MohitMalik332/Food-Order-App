db65d4254ae50bdf9d5bf9051feabbc3f8c6fe09
mohitmalik332 <malikmohit332@gmail.com.com>
2024-11-03 18:00:22 +0530

    functionality to show when an item is added to cart.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
+import { useContext } from 'react'
 import logoImg from '../assets/logo.jpg'
 import Button from './UI/Button'
+import CartContext from '../store/CartContext'
 
 export default function Header () {
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
+
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alt="header logo" />
             <h1>My Food App</h1>
         </div>
         <nav>
-            <Button textOnly={true}>Cart (0)</Button>
+            <Button textOnly={true}>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 }
